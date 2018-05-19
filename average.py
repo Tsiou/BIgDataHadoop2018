ee2ccca1c3e8e1eac96877cf1220b139a49ac40d
GeorgTsiou <georgtsiou@gmail.com>
2018-05-19 11:27:38 +0300

    swapped list values in average.py

--- average.py
+++ average.py
@@ -7,5 +7,5 @@ for line in sys.stdin:
     list += [int(s) for s in line.split() if s.isdigit()]
     
 
-average = math.floor(float(list[1])/float(list[0]))
+average = math.floor(float(list[0])/float(list[1]))
 print('%s' % average)
