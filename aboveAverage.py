2578b01c0014b206a5dd6bfa20ec313a971fff3b
GeorgTsiou <georgtsiou@gmail.com>
2018-05-19 20:47:31 +0300

    fixed aboveAverage (cycle 2)

--- aboveAverage.py
+++ aboveAverage.py
@@ -1,21 +1,21 @@
 from mrjob.job import MRJob
+import re
+
+WORD_RE = re.compile(r"[\w']+")
 
 class aboveAverage(MRJob):
 
     def mapper(self, _, line):
-        
-        words = line.split()
-        for word in words:
-            if len(word) > average:
-                yield (word, 1)
+        for word in WORD_RE.findall(line):
+            if len(word) > 5:
+                yield word, 1
 
     def combiner(self, word, counts):
         yield word, sum(counts)
 
     def reducer(self, word, counts):
         yield word, sum(counts)
-        
-
 
 if __name__ == '__main__':
     aboveAverage.run()
+
